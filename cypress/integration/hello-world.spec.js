a5acee836b931db5f351acb64b0596f7cf426fa7
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-10-18 09:14:30 +0200

    update cypress test

--- cypress/integration/hello-world.spec.js
+++ cypress/integration/hello-world.spec.js
@@ -1,6 +1,13 @@
 describe('Hello World', function() {
     before(function() {
+        cy.server();
+        cy.route({method: 'GET', url: `**/hello-world`, response: {payload: 'content'}}).as('HelloWorld');
         cy.viewport('macbook-15');
         cy.visit('index.html');
+    });
+
+    it('renders content', () => {
+        cy.wait('@HelloWorld');
+        cy.get('[data-test-id=hello-world]').contains('content');
     });
 });
