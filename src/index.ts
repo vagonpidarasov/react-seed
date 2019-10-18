4e7bfd24f90995d6f1da822af487e09300205a01
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-10-18 14:52:54 +0200

    add service worked for pwa

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import './index.scss';
 import {bootstrap} from 'src/app';
 bootstrap();
+
+window.addEventListener('load', function() {
+    if ('serviceWorker' in navigator) {
+        if (process.env.NODE_ENV === 'production') {
+            navigator.serviceWorker.register('/service-worker.js');
+        } else {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.unregister();
+            });
+        }
+    }
+});
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const {GenerateSW} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 const config = require('./webpack.config.js');
 
 module.exports = merge(config, {
@@ -10,4 +12,21 @@ module.exports = merge(config, {
     output: {
         path: path.resolve(__dirname, './prod'),
     },
+    plugins: [
+        new GenerateSW({
+            exclude: [/main\.css$/],
+        }),
+        new WebpackPwaManifest({
+            name: 'React Progressive Web App',
+            short_name: 'React PWA',
+            description: 'Some description',
+            background_color: '#ffffff',
+            icons: [
+                {
+                    src: path.resolve('icon.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                },
+            ]
+        })
+    ],
 });
